298f71e03908a7ce6255db034df82d77fd39a1e7
IniFadhil <rffadhil07@gmail.com>
2024-12-13 17:10:31 +0700

    Revisi CRUD Kategori

--- app/Http/Resources/Admin/CategoryResource.php
+++ app/Http/Resources/Admin/CategoryResource.php
@@ -16,7 +16,7 @@ class CategoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return[
+        return [
             'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
--- resources/js/Pages/Admin/Categories/Index.jsx
+++ resources/js/Pages/Admin/Categories/Index.jsx
@@ -12,15 +12,41 @@ import {
 } from '@/Components/ui/alert-dialog';
 import { Avatar, AvatarFallback, AvatarImage } from '@/Components/ui/avatar';
 import { Button } from '@/Components/ui/button';
-import { Card, CardContent } from '@/Components/ui/card';
+import { Card, CardContent, CardFooter, CardHeader } from '@/Components/ui/card';
+import { Input } from '@/Components/ui/input';
+import { Pagination, PaginationContent, PaginationItem, PaginationLink } from '@/Components/ui/pagination';
+import { Select, SelectContent, SelectItem, SelectTrigger } from '@/Components/ui/select';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/Components/ui/table';
+import { useFilter } from '@/hooks/useFilter';
 import AppLayout from '@/Layouts/AppLayout';
 import { flashMessage } from '@/lib/utils';
 import { Link, router } from '@inertiajs/react';
-import { IconCategory, IconPencil, IconPlus, IconTrash } from '@tabler/icons-react';
+import { SelectValue } from '@radix-ui/react-select';
+import { IconArrowsDownUp, IconCategory, IconPencil, IconPlus, IconRefresh, IconTrash } from '@tabler/icons-react';
+import { useState } from 'react';
 import { toast } from 'sonner';
 
+
+
 export default function Index(props) {
+    const {data: categories,  meta } = props.categories;
+    const [params, setParams] = useState(props.state);
+
+    const onSortable = (field) => {
+        setParams({
+            ...params,
+            field:field,
+            direction: params.direction == 'asc' ? 'desc' : 'asc',
+        })
+    }
+
+    useFilter({
+
+        route: route('admin.categories.index'),
+        Values: params,
+        only: ['categories'],
+    });
+
     return (
         <div className="flex w-full flex-col pb-32">
             <div className="mb-8 flex flex-col items-start justify-between gap-y-4 lg:flex-row lg:items-center">
@@ -39,22 +65,105 @@ export default function Index(props) {
             </div>
 
             <Card>
+                <CardHeader>
+                    <div className='flex w-full flex-col gap-4 lg:flex-row lg:items-center'>
+                        <Input
+                        className="w-full sm:w-1/4"
+                        placeholder="Search..."
+                        value={params?.search}
+                        onChange={(e) => {
+                            setParams((prev) => ({
+                            ...prev,
+                            search: e.target.value,
+                            }));
+                        }}
+                        />
+                        <Select value={params?.load} onValueChange={(e) => setParams({...params, load: e})}>
+                            <SelectTrigger className="w-full sm:w-24">
+                                <SelectValue placeholder='load'/>
+
+                            </SelectTrigger>
+                            <SelectContent>
+                                {[10, 25, 50, 75, 100].map((number, index)=>(
+                                    <SelectItem key={index} value={number}>
+                                        {number}
+                                    </SelectItem>
+                                    
+                                ))}
+                            </SelectContent>
+
+                        </Select>
+                        <Button
+                        variant="red"
+                        onClick={() => setParams(props.state)}
+                        size="xl"
+                        >
+                            <IconRefresh className="size-4"/>
+                            Bersihkan
+                        </Button>
+                    </div>
+                </CardHeader>
+
                 <CardContent className="px-0 py-0 [&-td]:whitespace-nowrap [&_td]:px-6 [&_th]:px-6">
                     <Table className="w-full">
                         <TableHeader>
                             <TableRow>
-                                <TableHead>#</TableHead>
-                                <TableHead>Nama</TableHead>
-                                <TableHead>Slug</TableHead>
+                                <TableHead>
+                                    <Button variant="ghost"
+                                    className="group inline-flex"
+                                    onClick={()=> onSortable('id')}
+                                    >
+
+                                        #
+                                        <span className="ml-2 flex-none rounded text-muted-foreground">
+                                            <IconArrowsDownUp className="size-4 text-muted-foreground"/>
+                                        </span>
+                                    </Button>
+                                </TableHead>
+                                <TableHead>
+                                    <Button variant="ghost"
+                                    className="group inline-flex"
+                                    onClick={()=> onSortable('name')}
+                                    >
+
+                                        Nama
+                                        <span className="ml-2 flex-none rounded text-muted-foreground">
+                                            <IconArrowsDownUp className="size-4 text-muted-foreground"/>
+                                        </span>
+                                    </Button>
+                                    </TableHead>
+                                <TableHead>
+                                <Button variant="ghost"
+                                    className="group inline-flex"
+                                    onClick={()=> onSortable('slug')}
+                                    >
+
+                                        Slug
+                                        <span className="ml-2 flex-none rounded text-muted-foreground">
+                                            <IconArrowsDownUp className="size-4 text-muted-foreground"/>
+                                        </span>
+                                    </Button>
+                                </TableHead>
                                 <TableHead>Cover</TableHead>
-                                <TableHead>Dibuat pada</TableHead>
+                                <TableHead>
+                                <Button variant="ghost"
+                                    className="group inline-flex"
+                                    onClick={()=> onSortable('created_at')}
+                                    >
+
+                                        Dibuat pada
+                                        <span className="ml-2 flex-none rounded text-muted-foreground">
+                                            <IconArrowsDownUp className="size-4 text-muted-foreground"/>
+                                        </span>
+                                    </Button>
+                                </TableHead>
                                 <TableHead>Aksi</TableHead>
                             </TableRow>
                         </TableHeader>
                         <TableBody>
-                            {props.categories.map((category, index) => (
+                            {categories.map((category, index) => (
                                 <TableRow key={index}>
-                                    <TableCell>{index + 1}</TableCell>
+                                    <TableCell>{index + 1 + (meta.current_page - 1) * meta.per_page}</TableCell>
                                     <TableCell>{category.name}</TableCell>
                                     <TableCell>{category.slug}</TableCell>
                                     <TableCell>
@@ -117,6 +226,32 @@ export default function Index(props) {
                         </TableBody>
                     </Table>
                 </CardContent>
+                <CardFooter className="flex flex-col items-center justify-between w-full py-2 border-t lg:flex-row">
+                        <p className='mb-2 text-sm text-muted-foreground'>
+                            Menampilkan <span className='font-medium text-orange-500'>{meta.from ?? 0}</span> dari {meta.total} kategori
+                        </p>
+
+                        <div className='overflow-x-auto'>
+                            {meta.has_pages && (
+
+                                <Pagination>
+                                    <PaginationContent className="flex flex-wrap justify-center lg:justify-end">
+                                        {meta.links.map((link, index)=> (
+                                            <PaginationItem key={index} className="mx-1 mb-1 lg:mb-0">
+                                                <PaginationLink
+                                                    href={link.url}
+                                                    isActive={link.active}
+                                                
+                                                >
+                                                    {link.label}
+                                                </PaginationLink>
+                                            </PaginationItem>
+                                        ))}
+                                    </PaginationContent>
+                                </Pagination>
+                            )}
+                        </div>
+                </CardFooter>
             </Card>
         </div>
     );
