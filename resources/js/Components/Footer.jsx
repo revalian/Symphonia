8fc278cdfdcc7c339a3a8f53f8483c5d3710dbdc
rivaldy913 <rivaldynovyandp@gmail.com>
2024-12-16 02:53:05 +0700

    Mengerjakan Footer

---
+++ resources/js/Components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="mt-8 bg-gray-800 py-6 text-white">
+            <div className="container mx-auto text-center">
+                <p>&copy; {new Date().getFullYear()} Symphonia. Harmoni dalam Setiap Nada.</p>
+            </div>
+        </footer>
+    );
+}
